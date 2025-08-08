ca24da0c3f7de638b1343909341ce3a8f4ef9ab0
Paul Siegel Nadiv <paul@pricetto.com>
2025-08-08 08:53:49 -0400

    actually made tiles 4x4

--- components/GameBoard.tsx
+++ components/GameBoard.tsx
@@ -123,7 +123,7 @@ export default function GameBoard({ puzzle, onComplete }: Props) {
           })}
         </div>
       )}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+      <div className="grid grid-cols-4 gap-2">
         {tiles.map((item: any, i: number) => {
           const isSelected = selection.includes(i);
           const isFound = found.includes(item.category);
